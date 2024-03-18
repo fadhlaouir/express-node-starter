b6be67fd79646f116752b8fb09d29a671fff608e
fadhlaouir <raed.fadhlaoui@hotmail.com>
2024-03-18 12:38:01 +0100

    chore: Update package.json

--- server.js
+++ server.js
@@ -46,8 +46,6 @@ app.use(cors());
 const userRoutes = require('./src/routes/user.route');
 const authRoutes = require('./src/routes/auth.route');
 
-
-
 // local APIs
 app.use('/v1/api', authRoutes);
 app.use('/v1/api', userRoutes);
